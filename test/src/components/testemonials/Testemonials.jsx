83875bc890f68c63c3967af3a69b424c0c31aef8
Nova Gas <novagas95@gmail.com>
2022-08-20 09:26:51 +0100

    testemonials but no swiper yet

--- test/src/components/testemonials/Testemonials.jsx
+++ test/src/components/testemonials/Testemonials.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './testemonials.css'
+import data from './data'
+
 
 const Testemonials = () => {
   return (
-    <section>Testemonials</section>
+    <section id='testemonials'>
+      <h5>Review From Clients</h5>
+      <h2>Testemonials</h2>
+      <div className="container testemonials__container">
+        {
+          data.map(
+            ({ avatar, name, review }, index) => {
+              return (
+                <article key={index} className="testemonial">
+                  <div className="client__avatar">
+                    <img src={avatar} alt="Avatar One" />
+                  </div>
+                  <h5> {name} </h5>
+                  <small className='client__review'>{review}
+                  </small>
+                </article>
+              )
+            }
+          )
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ test/src/components/testemonials/data.jsx
@@ -0,0 +1,24 @@
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+
+export default [
+    {
+        avatar:AVTR1,
+        name:"John Doe",
+        review:"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisut aliquip ex ea commodo consequat."
+    },
+
+    {
+        avatar:AVTR2,
+        name:"John Doe",
+        review:"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisut aliquip ex ea commodo consequat."
+    },
+
+    {
+        avatar:AVTR3,
+        name:"John Doe",
+        review:"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisut aliquip ex ea commodo consequat."
+    }
+]
+
