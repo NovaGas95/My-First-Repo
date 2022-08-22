fcc78cb55ddb4f66720ec8d02045cc0345886d66
Nova Gas <novagas95@gmail.com>
2022-08-22 11:07:34 +0100

    No Swiper Solution Found, but managed anyway.

--- test/src/components/testemonials/Testemonials.jsx
+++ test/src/components/testemonials/Testemonials.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
 import './testemonials.css'
 import data from './data'
-
+import { useState } from 'react'
 
 
 
 
 
 const Testemonials = () => {
+
+  const [slide, setSlide] = useState(data[0])
   return (
     <section id='testemonials'>
       <h5>Review From Clients</h5>
       <h2>Testemonials</h2>
       <div className="container testemonials__container">
 
-        {
-          data.map(
-            ({ avatar, name, review }, index) => {
-              return (
-                <article key={index} className="testemonial">
-                  <div className="client__avatar">
-                    <img src={avatar} alt="Avatar One" />
-                  </div>
-                  <h5> {name} </h5>
-                  <small className='client__review'>{review}
-                  </small>
-                </article>
-              )
-            }
+        {data.map(({ id, avatar, name, review }) => {
+          return (
+            <article key={id} className='testemonial'>
+              <div className='client__avatar'>
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>{review}</small>
+            </article>
           )
-        }
+        })}
+
+
+
+
       </div >
     </section>
   )
--- test/src/components/testemonials/data.jsx
+++ test/src/components/testemonials/data.jsx
@@ -2,20 +2,24 @@ import AVTR1 from '../../assets/avatar1.jpg'
 import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 
-export default [
+
+export default  [
     {
+        id:1,
         avatar:AVTR1,
         name:"John Doe",
         review:"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisut aliquip ex ea commodo consequat."
     },
 
     {
+        id:2,
         avatar:AVTR2,
         name:"John Doe",
         review:"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisut aliquip ex ea commodo consequat."
     },
 
     {
+        id:3,
         avatar:AVTR3,
         name:"John Doe",
         review:"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisut aliquip ex ea commodo consequat."
